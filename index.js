e7b884b65f7b4f047e334ce067acfd6019d32405
dwleonhardt <dwleonhardt@gmail.com>
2018-02-22 14:44:19 -0700

    basic app setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const shopifyKey = process.env.SHOPIFY_KEY;
+const shopifySecret = process.env.SHOPIFY_SECRET;
+const forwardingAddress = "http://d5285b39.ngrok.io";
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -9,4 +12,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.listen(port, () => {
   console.log('Listening on port', port);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
